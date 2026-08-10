69f37eeb46130cd81c0eabb30c85196608f5a6cd
Yazmin Ramirez <yazzy06ramirez@aol.com>
2026-08-10 19:07:07 -0400

    Refactor App.jsx for improved readability and consistency

--- globe-guide-app/src/App.jsx
+++ globe-guide-app/src/App.jsx
@@ -1,30 +1,37 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import CountryDetails from "./components/CountryDetails";
-import Favorites from "./components/Favorites.jsx";
+import Favorites from "./components/Favorites";
 import Footer from "./components/Footer";
+
 import "./App.css";
+
 function Countries() {
   return <h1>Explore Countries 🌍</h1>;
 }
+
 function About() {
   return <h1>About GlobeGuide ℹ️</h1>;
 }
+
 function App() {
   return (
     <BrowserRouter>
-      {" "}
-      <Navbar />{" "}
+      <Navbar />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/countries" element={<Countries />} />
         <Route path="/country/:name" element={<CountryDetails />} />
         <Route path="/favorites" element={<Favorites />} />
         <Route path="/about" element={<About />} />
-      </Routes>{" "}
-      <Footer />{" "}
+      </Routes>
+
+      <Footer />
     </BrowserRouter>
   );
 }
+
 export default App;
