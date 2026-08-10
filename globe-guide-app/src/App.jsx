45a856683b6a6d02c62dd5fc2a7fc4acd1a450fa
Valentina BM <valentina15bermo@gmail.com>
2026-08-10 19:22:43 -0400

    fix

--- globe-guide-app/src/App.jsx
+++ globe-guide-app/src/App.jsx
@@ -1,8 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
-import Favorites from "./components/Favorites.jsx";
+import Favorites from "./components/Favorites";
 import Footer from "./components/Footer";
+
+
 import "./App.css";
 function Countries() {
   return <h1>Explore Countries 🌍</h1>;
--- globe-guide-app/src/components/Favorites.jsx
+++ globe-guide-app/src/components/Favorites.jsx
@@ -1,20 +1,19 @@
-function Favorites ({favorites}) {
-    return (
-        <main>
-            <h1>My Favorites</h1>
+import CountryCard from "./CountryCard";
 
-            {favorites.length === 0 ? (
-                <p>You haven't saved any countries yet</p>
-            ) : (
-                favorites.map((country) => (
-                    <CountryCard
-                    key={country.cca3}
-                    country={country}
-                    />
-                ))
-            )}
-        </main>
-    );
+function Favorites({ favorites = [] }) {
+  return (
+    <main>
+      <h1>My Favorites</h1>
+
+      {favorites.length === 0 ? (
+        <p>You haven't saved any countries yet</p>
+      ) : (
+        favorites.map((country) => (
+          <CountryCard key={country.cca3} country={country} />
+        ))
+      )}
+    </main>
+  );
 }
 
 export default Favorites;
