1046b0dc37aa281d27888b51b778690dec03a4ab
Valentina BM <valentina15bermo@gmail.com>
2026-08-11 09:47:54 -0400

    card

--- globe-guide-app/src/components/CountryCard.jsx
+++ globe-guide-app/src/components/CountryCard.jsx
@@ -1,14 +1,22 @@
-function CountryCard ({country}) {
-    return (
+import { Link } from "react-router-dom";
+
+function CountryCard({ country }) {
+  return (
     <div className="country-card">
       <img
         src={country.flags.png}
         alt={`${country.name.common} flag`}
       />
+
       <h2>{country.name.common}</h2>
+
       <p>Region: {country.region}</p>
-      <p>Capital: {country.capital?.[0]}</p>
-      <button>View Details</button>
+
+      <p>Capital: {country.capital?.[0] || "N/A"}</p>
+
+      <Link to={`/country/${country.name.common}`}>
+        View Details
+      </Link>
     </div>
   );
 }
