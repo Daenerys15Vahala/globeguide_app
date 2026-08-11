81a629eb6f68d5b35eead16dab71dde25a3fd7bf
Yazmin Ramirez <yazzy06ramirez@aol.com>
2026-08-11 09:43:15 -0400

    Refactor CountryCard component to use correct country name properties and simplify JSX structure

--- globe-guide-app/src/components/CountryCard.jsx
+++ globe-guide-app/src/components/CountryCard.jsx
@@ -3,20 +3,15 @@ import { Link } from "react-router-dom";
 function CountryCard({ country }) {
   return (
     <div className="country-card">
-      <img
-        src={country.flags.png}
-        alt={`${country.name.common} flag`}
-      />
+      <img src={country.flags.png} alt={`${country.name} flag`} />
 
-      <h2>{country.name.common}</h2>
+      <h2>{country.name}</h2>
 
       <p>Region: {country.region}</p>
 
-      <p>Capital: {country.capital?.[0] || "N/A"}</p>
+      <p>Capital: {country.capital || "N/A"}</p>
 
-      <Link to={`/country/${country.name.common}`}>
-        View Details
-      </Link>
+      <Link to={`/country/${country.name}`}>View Details</Link>
     </div>
   );
 }
