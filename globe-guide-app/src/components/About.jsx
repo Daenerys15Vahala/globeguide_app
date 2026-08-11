55a9be2c141cc66d652ed104a5f463cefb6ef5ff
Valentina BM <valentina15bermo@gmail.com>
2026-08-10 21:46:42 -0400

    about

--- globe-guide-app/src/components/About.jsx
+++ globe-guide-app/src/components/About.jsx
@@ -15,15 +15,21 @@ function About() {
             <section className="about-section">
                 <h2>What Can You Explore?</h2>
 
-            <div className="about-features">
-                <p>US Country Information</p>
-                <p>Languages</p>
-                <p>Currencies</p>
-                <p>Population</p>
-                <p>🕐Time Zones</p>
-                <p>📍Lcations</p>
-            </div>
-          </section>
+                <div className="about-features">
+                    <p>US Country Information</p>
+                    <p>🗣️Languages</p>
+                    <p>💰Currencies</p>
+                    <p>👥Population</p>
+                    <p>🕐Time Zones</p>
+                    <p>📍Lcations</p>
+                </div>
+            </section>
+
+            <section className="about-section">
+                <h2>Why GlobeGuide</h2>
+                <p>GlobeGuide brings useful information about countries together in one easy.</p>
+                <p>Whether you're curious about a country's capital, language, population, or location</p>
+            </section>
         </main>
     )
 }
