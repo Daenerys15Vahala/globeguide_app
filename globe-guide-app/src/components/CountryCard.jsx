b235f335f4b8c4ab72ef07c1195b6de5c9702869
Valentina BM <valentina15bermo@gmail.com>
2026-08-11 10:49:49 -0400

    favorites

--- globe-guide-app/src/components/CountryCard.jsx
+++ globe-guide-app/src/components/CountryCard.jsx
@@ -1,45 +1,35 @@
 
 import { Link } from "react-router-dom";
 
-function CountryCard({ country, favorites, setFavorites }) {
-    const isFavorite = favorites.some((favorite) => favorite.name === country.name);
+function CountryCard({ country, favorites = [], setFavorites = () => {} }) {
+  const countryName = country?.name?.common || country?.name || "Unknown";
+  const isFavorite = favorites.some((favorite) => favorite?.name?.common === countryName || favorite?.name === countryName);
 
-    function handleFavorite() {
-        if (isFavorite) {
-            setFavorites(
-                favorites.filer(
-                    (favorite) => favorite.name !== country.name
-                )
-            );
-        } else {
-            setFavorites([...favorites, country]);
-        }
+  function handleFavorite() {
+    if (isFavorite) {
+      setFavorites(favorites.filter((favorite) => (favorite?.name?.common || favorite?.name) !== countryName));
+    } else {
+      setFavorites([...favorites, { ...country, name: { common: countryName } }]);
     }
+  }
 
-    return (
-        <div className="country-card">
+  return (
+    <div className="country-card">
+      <button className="fav-button" onClick={handleFavorite}>
+        {isFavorite ? "❤️" : "🤍"}
+      </button>
 
-            <button className="fav-button"
-                onClick={handleFavorite}>
-                {isFavorite ? "❤️" : "🤍"}
-            </button>
+      <img src={country.flags?.png || country.flag} alt={`${countryName} flag`} />
 
-            <img
-                src={country.flags?.png || country.flag}
-                alt={`${country.name} flag`}
-            />
+      <h2>{countryName}</h2>
 
-            <h2>{country.name}</h2>
+      <p>Region: {country.region || "N/A"}</p>
 
-            <p>Region: {country.region || "N/A"}</p>
+      <p>Capital: {country.capital?.[0] || "N/A"}</p>
 
-            <p>Capital: {country.capital || "N/A"}</p>
-
-            <Link to={`/country/${country.name}`}>
-                View Details
-            </Link>
-        </div>
-    );
+      <Link to={`/country/${encodeURIComponent(countryName)}`}>View Details</Link>
+    </div>
+  );
 }
 
 export default CountryCard;
