de0b6d1a77ba1dcb6a9e2f68f6970868f7f9553f
Valentina BM <valentina15bermo@gmail.com>
2026-08-10 21:17:40 -0400

    changes

--- globe-guide-app/src/components/CountryCard.jsx
+++ globe-guide-app/src/components/CountryCard.jsx
@@ -1,22 +1,14 @@
 function CountryCard ({country}) {
     return (
     <div className="country-card">
-
       <img
         src={country.flags.png}
         alt={`${country.name.common} flag`}
       />
-
       <h2>{country.name.common}</h2>
-
       <p>Region: {country.region}</p>
-
-      <p>
-        Capital: {country.capital?.[0]}
-      </p>
-
+      <p>Capital: {country.capital?.[0]}</p>
       <button>View Details</button>
-
     </div>
   );
 }
